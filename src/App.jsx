6268e7ac316a16a64b9a410c7fad22d89fd8b773
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-09-04 15:55:12 +0300

    yuyuyu

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Login from "./routes/Login";
 import Register from "./routes/Register";
 import ResetPassword from "./routes/ResetPassword";
 import Dashboard from "./routes/Dashboard";
-import AuthProvider from "./providers/AuthProvider";
 import EmployeeJoin from "./routes/EmployeeJoin";
 
 export default function App() {
@@ -16,14 +15,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/employee-join" element={<EmployeeJoin />} />
-        <Route
-          path="/dashboard/*"
-          element={
-            <AuthProvider>
-              <Dashboard />
-            </AuthProvider>
-          }
-        />
+        <Route path="/dashboard/*" element={<Dashboard />} />
         <Route path="*" element={<>404 page not found</>} />
       </Routes>
     </div>
