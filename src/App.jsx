c9e5488bc52900c7857214fac49f0f295d32bf9e
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-09-04 16:32:26 +0300

    yuuy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
-import Register from "./routes/Register";
-// import ResetPassword from "./routes/ResetPassword";
-// import EmployeeJoin from "./routes/EmployeeJoin";
-// import Dashboard from "./routes/Dashboard";
+// import Register from "./routes/Register";
+import ResetPassword from "./routes/ResetPassword";
+import Dashboard from "./routes/Dashboard";
+import AuthProvider from "./providers/AuthProvider";
+import EmployeeJoin from "./routes/EmployeeJoin";
 
 export default function App() {
   return (
@@ -12,10 +13,17 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        {/* <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/employee-join" element={<EmployeeJoin />} /> */}
-        {/* <Route path="/dashboard/*" element={<Dashboard />} /> */}
+        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/employee-join" element={<EmployeeJoin />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            <AuthProvider>
+              <Dashboard />
+            </AuthProvider>
+          }
+        />
         <Route path="*" element={<>404 page not found</>} />
       </Routes>
     </div>
