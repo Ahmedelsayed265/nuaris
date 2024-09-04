d07a9f8a7366581796809eed643b5ca9ac8aab7a
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-09-04 16:26:10 +0300

    uyu7

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Login from "./routes/Login";
-// import Register from "./routes/Register";
-// import ResetPassword from "./routes/ResetPassword";
+import Register from "./routes/Register";
+import ResetPassword from "./routes/ResetPassword";
+import EmployeeJoin from "./routes/EmployeeJoin";
 // import Dashboard from "./routes/Dashboard";
-// import EmployeeJoin from "./routes/EmployeeJoin";
 
 export default function App() {
   return (
@@ -13,8 +13,8 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        {/* <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/employee-join" element={<EmployeeJoin />} /> */}
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/employee-join" element={<EmployeeJoin />} />
         {/* <Route path="/dashboard/*" element={<Dashboard />} /> */}
         <Route path="*" element={<>404 page not found</>} />
       </Routes>
